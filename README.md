31bbdb735d8d9126e836acd3b44fac2a2ef84278
kakimotoShunya <b7080@oic.jp>
2019-07-09 01:06:08 +0900

    LINE webhook functionの実装とデプロイ
    NoSQLに{LINE_uid: 名前} で登録する処理

--- README.md
+++ README.md
@@ -13,10 +13,29 @@ $ npm run dev
 
 # build for production and launch server
 $ npm run build
-$ npm start
 
 # generate static project
 $ npm run generate
+$ firebase deploy
+
+# function modifyed
+$ firebase deploy --only functions
+$ firebase deploy --only functions:<function name>
+
+# functions local test
+$ cd functions
+$ functions-emulator start
+$ functions-emulator deploy <function name> -H
+
 ```
 
 For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
+
+
+
+# まだなとこ
+- LINE通知
+  - LINE アプリを作成
+  - 携帯から表示名入力でDBにUIDと一緒に保存
+  - スケジュールページから送信機能実装
+- 参加メンバーをテーブルから読み込む
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,78 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+require('dotenv').config();
+var request = require("request");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// db connect
+if (!admin.apps.length) {
+  admin.initializeApp()
+}
+
+// line function
+const sendMessage = (message, replyToken) => {
+  let body = JSON.stringify({
+    type: 'message',
+    replyToken: replyToken,
+    messages: [{
+      type: "text",
+      text: message
+    }]
+  });
+  let headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer '+process.env.LINE_CHANNEL_ACCESS_TOKEN
+  }
+  return new Promise((resolve, reject ) => {
+    request.post({
+      url: 'https://api.line.me/v2/bot/message/reply',
+      headers: headers,
+      body: body
+    },(err, response, body)=> {
+      console.log('reply'+ body);
+    });
+  });
+}
+
+
+exports.helloWorld = functions.region('asia-northeast1').https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+exports.testdatabase = functions.region('asia-northeast1').https.onRequest((request, response) => {
+  const table = admin.database().ref('events/')
+  table.once("value").then(data => { response.send(data) } )
+});
+exports.lineWebhook = functions.region('asia-northeast1').https.onRequest((request, response) => {
+  console.log(request.body.events);
+  let event = request.body.events[0];
+  let replyToken = request.body.events[0].replyToken
+  if (event.type == 'message') {
+    console.log('message recieve');
+    if(event.message.text.match(/登録/)){
+      console.log('register process');
+      const lineId =event.source.userId;
+      let name = event.message.text.replace('登録', '').replace(' ', '').replace('　', '');
+      let db = admin.database();
+      db.ref('users/'+lineId).once("value").then(user => {
+        if(null == user.val()){
+          let updates = {};
+          updates[lineId] = { name: name };
+          admin.database().ref('users/').update(updates);
+          console.log('save', updates);
+          sendMessage(name+'さんを登録しました!', replyToken);
+        }else{
+          console.log('user exists', lineId);
+          sendMessage(name+'さんは登録されていました!', replyToken);
+        }
+      });
+    }else {
+      console.log('general message');
+      console.log(event.message.text);
+      sendMessage(event.message.text + 'には対応できていません。', replyToken);
+    }
+    
+  }else if( event.type = "follow"){
+    console.log('follow event');
+  }else{
+    console.log('other event')
+  }
+});
